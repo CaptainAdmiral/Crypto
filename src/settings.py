848ca0e695773b59870db9c9a4dc80d497912539
CaptainAdmiral <mattb1197@live.co.uk>
2022-11-27 16:44:55 +0000

    Added settings

---
+++ src/settings.py
@@ -0,0 +1,6 @@
+NETWORK_DELAY = 0.2 
+''' The average delay in seconds it takes for a net connection to successfuly propogate a packet to the target node '''
+NETWORK_DELAY_VARIABILITY = 0.1
+''' The ratio of standard deviation of the network delay to the mean delay
+
+e.g a value of 0.1 will result in a standard deviation that's 1/10th of the mean delay '''
